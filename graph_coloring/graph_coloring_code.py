4b0acc8e90638357bfc456125f8d9cc7645b6263
magojun <magojun@hotmail.com>
2023-06-05 00:02:10 -0300

    expanding quantum version of graph coloring.

--- graph_coloring/graph_coloring_code.py
+++ graph_coloring/graph_coloring_code.py
@@ -8,7 +8,7 @@ from docplex.mp.model import Model
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
 from qiskit_optimization.translators import from_docplex_mp
 
-from qiskit import Aer
+from qiskit import BasicAer
 from qiskit.algorithms import QAOA
 from qiskit.utils import algorithm_globals
 from qiskit.utils import QuantumInstance
@@ -59,12 +59,14 @@ nx.draw(G, node_color=color_map, node_size=600, with_labels=True)
 plt.savefig("classical_graph_coloring.png", format="PNG")
 plt.clf()
 
+
 def draw_graph(n, edges):
     G = nx.Graph()
-    G.add_nodes_from([i for i in range(1, n+1)])
+    G.add_nodes_from([i for i in range(1, n + 1)])
     G.add_edges_from(edges)
     nx.draw(G, with_labels=True)
     return G
+
 
 n = 5
 edges = [(1, 2), (1, 5), (2, 3), (2, 4), (2, 5), (3, 4), (4, 5)]
@@ -75,36 +77,62 @@ plt.clf()
 num_color = 3
 P = 4
 
-num_nodes = G. number_of_nodes()
+num_nodes = G.number_of_nodes()
 Q = np.eye(num_nodes * num_color)
 for i in range(1, num_nodes + 1):
     l = num_color * i + 1
-    for j in range(l-num_color, l):
-        for k in range(l-num_color, l):
+    for j in range(l - num_color, l):
+        for k in range(l - num_color, l):
             if k == j:
                 Q[j - 1][k - 1] = -P
             else:
-                Q[j-1][k-1] = P
+                Q[j - 1][k - 1] = P
 
 for i, j in G.edges:
-    for k in range (1, num_color+1):
+    for k in range(1, num_color + 1):
         # (node - 1) * num_color + k
-        m = (i-1) * num_color + k
-        o = (j-1) * num_color + k
-        Q[m-1][o-1] = P/2
-        Q[o-1][m-1] = P/2
+        m = (i - 1) * num_color + k
+        o = (j - 1) * num_color + k
+        Q[m - 1][o - 1] = P / 2
+        Q[o - 1][m - 1] = P / 2
 
 mdl = Model(name="Graph Coloring")
 x = [mdl.binary_var('x%s' % i) for i in range(len(Q))]
-objective = mdl.sum([x[i]*Q[i, j]*x[j] for i in range(len(Q)) for i in range(len(Q))])
+objective = mdl.sum([x[i] * Q[i, j] * x[j] for i in range(len(Q)) for i in range(len(Q))])
 mdl.minimize(objective)
 qp = from_docplex_mp(mdl)
 
 print(qp.prettyprint())
 
-
 seed = 1234
 algorithm_globals.random_seed = seed
-quantum_instance = QuantumInstance(backend=Aer.get_backend('qasm_simulator'), shots=10000)
-#qaoa = MinimumEigenOptimizer(min_eigen_solver=QAOA(reps=3, quantum_instance=quantum_instance))
-#qaoa_result = qaoa.solve(qp)
+quantum_instance = QuantumInstance(backend=BasicAer.get_backend('qasm_simulator'), shots=10000)
+qaoa = MinimumEigenOptimizer(min_eigen_solver=QAOA(reps=3, quantum_instance=quantum_instance))
+qaoa_result = qaoa.solve(qp)
+
+
+def assign_color(result, num_color):
+    colors = ['BLUE', 'GREEN', 'RED', 'YELLOW', 'ORANGE', 'PINK', 'BLACK', 'BROWN', 'WHITE', 'PURPLE', 'VIOLET']
+    color_map = list()
+    for pos, val in enumerate(result.x):
+        if val == 1:
+            node = pos // num_color + 1
+            color = pos % num_color
+            color_map.append(colors[color])
+            print("for node %s with binary variable %s the assigned color is %s" % (node, pos, colors[color]))
+    return color_map
+
+
+qaoa_color_map = assign_color(qaoa_result, 3)
+
+
+def draw_color_graph(n, edges, color_map):
+    G = nx.Graph()
+    G.add_nodes_from([i for i in range(1, n + 1)])
+    G.add_edges_from(edges)
+    nx.draw(G, node_color=color_map, node_size=600, with_labels=True)
+
+
+draw_color_graph(n, edges, qaoa_color_map)
+plt.savefig("quantum_graph_coloring.png", format="PNG")
+plt.clf()
